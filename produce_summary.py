66fc3dd2a02b010b7bd7546773e00bc2dafb40bf
Wooyang Son <sonwy102@gmail.com>
2020-10-01 00:06:25 +0000

    Add produce_summary function

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,37 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def produce_summary(day, filename):
+    """Generate report of melon deliveries for a specified day
+    
+    produce_summary takes in the day and the report txt file, iterates
+    through each line of report, processes the data, and prints out 
+    delivery report for that day.
+    """
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    # Print day number as header
+    print(f"Day {day}")
 
-    print("Delivered {} {}s for total of ${}".format(
+    # Open file
+    report = open(filename)
+
+    # Iterate through each line of txt file
+    for line in filename:
+
+        # Strip last space off and split string input to a list delimited by '|'
+        line = line.rstrip()
+        words = line.split('|')
+
+        # Store melon name, count sold, and total amount sold in $ into variables
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        # Print out report of deliveries in a formatted fashion
+        print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
-the_file.close()
+    
+    # Close report
+    report.close()
 
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+# Calling produce_summary for days 1, 2, 3 to generate their delivery reports
+produce_summary(1, "um-deliveries-20140519.txt")
+produce_summary(2, "um-deliveries-20140520.txt")
+produce_summary(3, "um-deliveries-20140521.txt")
